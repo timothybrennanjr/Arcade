2acf120a5b33f1e4c5ce19039bebfa989a0e66ba
timothy <timothybrennan8@gmail.com>
2022-10-05 15:28:23 -0400

    I added a border and end game alert. Also made it to where you can move my snake.

--- index.js
+++ index.js
@@ -10,29 +10,72 @@ const food = board.getContext("2d");
 // (let x = board.width / 2) because x goes sideways on the axis.
 let snakeX = board.width / 2;
 // (let y = board.height) because goes up on the axis.
-let snakeY = board.height - 100;
+let snakeY = board.height - 300;
+
+let foodX = board.width / 4;
+let foodY = board.height + 200;
+
 
 // the direction on the axis
 let snakeRight = 20;
-let snakeLeft = -20;
+let snakeUp = -20;
 
-function snakeMove() {
-  // .clearReact(0, 0, board.width, board.height); just resets the default parameters from the board.
-  snake.clearRect(0, 0, board.width, board.height);
+
+// button to start game
+
+alert('Start Game');
+  
+
+window.onload = function(){
+    document.addEventListener('keydown', (e) =>{
+      keyPress(e)  
+    })
+}
+
+function keyPress (e){
+    if(e.key === "ArrowRight"){
+        snakeX = snakeX + 20
+    }
+    if(e.key === "ArrowLeft"){
+        snakeX = snakeX + -20
+    }
+    if(e.key === "ArrowUp"){
+        snakeY = snakeY - 20
+    }
+    if(e.key === "ArrowDown"){
+        snakeY = snakeY + 20
+    }
+}
+
+
+function foodSpawn() {
+    snake.beginPath();
+    snake.rect(foodX, foodY, 20, 20);
+    snake.fillStyle = "White";
+    snake.fill();
+    snake.closePath();
+  }
+
+function snakeLooks() {
   snake.beginPath();
   snake.rect(snakeX, snakeY, 20, 20);
   snake.fillStyle = "yellow";
   snake.fill();
   snake.closePath();
-
-  snakeY += snakeLeft;
 }
-setInterval(snakeMove, 200);
 
-function allThingsFood() {
-  food.beginPath();
-  food.rect(50, 50, 20, 20);
-  food.fillStyle = "red";
-  food.fill();
-  food.closePath();
-}
+function drawSnake(){
+    // this resets the block the snake is on before so it looks like it is moving.
+    snake.clearRect(0, 0, board.width, board.height);
+    snakeLooks();
+    // dx += snakeRight;
+    // dy += snakeUp;
+    if (snakeY < 0 || snakeY > board.height -20 || snakeX < 0 || snakeX > board.width-20){
+        endGame()
+    }
+    function endGame (){
+        alert("game over")
+    }
+} 
+
+setInterval(drawSnake, 100);
