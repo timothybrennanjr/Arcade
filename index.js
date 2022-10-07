b181f61137e6a138d1dfc7acb2be2df38f7de792
timothy <timothybrennan8@gmail.com>
2022-10-07 15:28:27 -0400

    fixed my spawn food function.

--- index.js
+++ index.js
@@ -12,49 +12,61 @@ let snakeX = board.width / 2;
 // (let y = board.height) because goes up on the axis.
 let snakeY = board.height - 300;
 
-let foodX = board.width / 4;
-let foodY = board.height + 200;
+let foodX = 20;
+let foodY = 580;
 
+let snakeSizeWidth = 20
+let snakeSizeHeight = 20
 
 // the direction on the axis
 let snakeRight = 20;
 let snakeUp = -20;
 
+window.onload = function () {
+  document.addEventListener("keydown", (e) => {
+    keyPress(e);
+  });
+};
 
-// button to start game
-
-alert('Start Game');
-  
-
-window.onload = function(){
-    document.addEventListener('keydown', (e) =>{
-      keyPress(e)  
-    })
+function startGame(){
+foodSpawn()
+snakeSpawn()
 }
 
-function keyPress (e){
-    if(e.key === "ArrowRight"){
-        snakeX = snakeX + 20
-    }
-    if(e.key === "ArrowLeft"){
-        snakeX = snakeX + -20
-    }
-    if(e.key === "ArrowUp"){
-        snakeY = snakeY - 20
-    }
-    if(e.key === "ArrowDown"){
-        snakeY = snakeY + 20
-    }
+foodSpawn();
+
+function changeFood(){
+    min = 20
+    max = 580
+    return Math.random() * (max - min) + min;
 }
 
+
+function keyPress(e) {
+  if (e.key === "ArrowRight") {
+    snakeX = snakeX + 20;
+  }
+  if (e.key === "ArrowLeft") {
+    snakeX = snakeX + -20;
+  }
+  if (e.key === "ArrowUp") {
+    snakeY = snakeY - 20;
+  }
+  if (e.key === "ArrowDown") {
+    snakeY = snakeY + 20;
+  }
+  addToSnake()
+}
 
 function foodSpawn() {
-    snake.beginPath();
-    snake.rect(foodX, foodY, 20, 20);
-    snake.fillStyle = "White";
-    snake.fill();
-    snake.closePath();
-  }
+food.clearRect(0, 0, board.width, board.height);
+  food.beginPath();
+  food.rect(changeFood(), changeFood(), 20, 20);
+  food.fillStyle = "White";
+  food.fill();
+  food.closePath();
+}
+
 
 function snakeLooks() {
   snake.beginPath();
@@ -64,18 +76,32 @@ function snakeLooks() {
   snake.closePath();
 }
 
-function drawSnake(){
-    // this resets the block the snake is on before so it looks like it is moving.
-    snake.clearRect(0, 0, board.width, board.height);
-    snakeLooks();
-    // dx += snakeRight;
-    // dy += snakeUp;
-    if (snakeY < 0 || snakeY > board.height -20 || snakeX < 0 || snakeX > board.width-20){
-        endGame()
-    }
-    function endGame (){
-        alert("game over")
-    }
-} 
+function snakeSpawn() {
+  // this resets the block the snake is on before so it looks like it is moving.
+  snakeLooks();
+  snake.clearRect(0, 0, 0, 0);
+  // snakeX += snakeRight;
+//   snakeY += snakeUp;
+  if (
+    snakeY < 0 ||
+    snakeY > board.height - 20 ||
+    snakeX < 0 ||
+    snakeX > board.width - 20
+  ) {
+    endGame();
+  }
+  
+}
+setInterval(snakeSpawn, 100);
 
-setInterval(drawSnake, 100);
+function endGame() {
+    alert("game over");
+  }
+
+function addToSnake(){
+    if (snakeX === foodX && snakeY === foodY){
+        snakeSizeHeight = snakeSizeHeight * 2;
+        foodSpawn()
+    }
+}
+
